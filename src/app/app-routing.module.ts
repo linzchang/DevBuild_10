a6650b597f2c5bb4ed1c82ba60fa955569df1d0d
Lin-z Chang <lin-zchang@amrock.com>
2019-01-11 13:33:07 -0500

    Created social posts component, added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SocialPostsComponent } from './social-posts/social-posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SocialPostsComponent },
+  { path: 'posts', component: SocialPostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/models/post.model.ts
@@ -0,0 +1,4 @@
+export class PostModel {
+    subject: string;
+    body: string;
+}
