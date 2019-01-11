b8ad094ba579a5a0dc849c14b677bfca6e2f782b
Lin-z Chang <lin-zchang@amrock.com>
2019-01-11 13:41:51 -0500

    Created post class, created post list and added styling

--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostModel } from 'src/models/post.model';
 
 @Component({
   selector: 'app-social-posts',
@@ -9,7 +10,14 @@ export class SocialPostsComponent implements OnInit {
 
   constructor() { }
 
+  postList: PostModel[];
+
   ngOnInit() {
+    this.postList = [
+      { subject: "Dog", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor." },
+      { subject: "Cat", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor." },
+      { subject: "Fish", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor." }
+    ];
   }
 
 }
